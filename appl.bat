c7fa58067aa36b4a0a4f2aab8a0ac3fdb2c772c7
K. R. Bharat <83277978+KRBharat-DS@users.noreply.github.com>
2025-04-19 12:02:28 +0530

    Add files via upload

---
+++ appl.bat
@@ -0,0 +1,6 @@
+@echo off
+CALL C:\Users\bhara\miniconda3\Scripts\activate.bat resume_agent
+cd C:\Users\bhara\Desktop\Resume_JD_Matcher-main\Resume_JD_Matcher-main
+streamlit run streamlit_app.py
+
+
